06a8e1780ba3b7e58c106c50ea65927b3ed026c0
Pieter-Jan Baert <pjbaert@users.noreply.github.com>
2023-07-27 10:35:22 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,4 +1,12 @@
 if ("serviceWorker" in navigator) {
+  navigator.serviceWorker.getRegistrations().then(function (registrations) {
+    //returns installed service workers
+    if (registrations.length) {
+      for(let registration of registrations) {
+        registration.unregister();
+      }
+    }
+  });
   window.addEventListener("load", function() {
     navigator.serviceWorker
       .register("/serviceWorker.js")
