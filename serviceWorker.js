eaabc6dca35138e7772dea1debd02af3ea078e7f
Pieter-Jan Baert <pieter-jan.baert@calibrate.be>
2024-06-15 14:30:42 +0200

    added logic to remove outdated local caches.

--- serviceWorker.js
+++ serviceWorker.js
@@ -4,16 +4,35 @@ const assets = [
   "/index.html",
   "/app.js",
   "/favicon.ico",
-  "favicon-32x32.png"
+  "/favicon-16x26.png",
+  "/favicon-32x32.png",
+  "/mstile-150x150.png"
 ]
 
 
 self.addEventListener("install", installEvent => {
   installEvent.waitUntil(
     caches.open(staticPjbaert).then(cache => {
-      cache.addAll(assets)
+      cache.addAll(assets).then(() => self.skipWaiting())
     })
   )
+})
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(
+          cache => {
+            if (cache !== staticPjbaert) {
+              console.log('Service Worker: Clearing Old Cache');
+              return caches.delete(cache);
+            }
+          }
+        )
+      )
+    })
+  );
 })
 
 self.addEventListener("fetch", fetchEvent => {
